727623dc22485318d20142664dbf8439e7a203aa
Guilherme Deschamps <guilhermerd6@hotmail.com>
2021-04-02 23:52:20 -0300

    Implementado errorMessage para os erros do sistema

---
+++ coachtv/backend/backend/src/main/java/udesc/pin3/ErrorMessage.java
@@ -0,0 +1,18 @@
+package udesc.pin3;
+
+public class ErrorMessage {
+
+    String errorMessage;
+
+    public ErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
